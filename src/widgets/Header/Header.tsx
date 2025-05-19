b84b845014b3a0223df823810a3e800cca1b8846
ArthurAkhmetov <arthur.akhmetov.2008@gmail.com>
2025-05-19 22:48:53 +0300

    add(widgets): header and sidebar

--- src/widgets/Header/Header.tsx
+++ src/widgets/Header/Header.tsx
@@ -1,3 +1,32 @@
+import { Button } from '@/shared/UI/button'
+import { Input } from '@/shared/UI/input'
+import { Moon, Search, Sun } from 'lucide-react'
+import { useState } from 'react'
+import s from './header.module.scss'
+
 export const Header = () => {
-	return <header>Header</header>
+	const [isDarkTheme, setIsDarkTheme] = useState(false)
+
+	return (
+		<header className={`${s.header} container`} role='header'>
+			<section className={s.searchBlock} role='search-anime'>
+				<Input type='text' name='search' className={s.searchAnime} />
+				<Search className={s.loupe} />
+			</section>
+
+			<section className={s.toggleTheme} role='toggle-theme'>
+				<Button>EN</Button>
+				<Button>RU</Button>
+			</section>
+
+			<section role='change-language'>
+				<Button
+					variant={isDarkTheme ? 'outline' : 'default'}
+					onClick={() => setIsDarkTheme((prev) => !prev)}
+				>
+					{isDarkTheme ? <Moon /> : <Sun />}
+				</Button>
+			</section>
+		</header>
+	)
 }
